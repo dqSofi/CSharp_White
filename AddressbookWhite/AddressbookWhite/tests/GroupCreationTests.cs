8fe71cda6bbe5879cc76213d1f18ebcac54c135d
dqSofi <dq.sofi@gmail.com>
2017-11-25 15:36:23 +0300

    Создан работающий тест создания группы1

---
+++ AddressbookWhite/AddressbookWhite/tests/GroupCreationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AddressbookWhite
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData newGroup = new GroupData()
+            {
+                Name = "White"
+            };
+            app.Groups.Add(newGroup);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            //Assert.AreEqual(oldGroups, newGroup);
+
+        }
+    }
+}
